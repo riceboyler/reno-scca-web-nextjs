a994dd35f9a562d0acb47e967e2b80fd83a4a778
Jason Clark <riceboylerdj@gmail.com>
2026-01-22 12:19:10 -0800

    Gallery Page updates

--- src/app/api/gallery/[eventId]/route.ts
+++ src/app/api/gallery/[eventId]/route.ts
@@ -2,10 +2,11 @@ import { getConnection } from "@/db/db";
 import type { GalleryModel } from "@/db/models/Gallery.model";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(_req: Request, ctx: RouteContext<'/api/gallery/[eventId]'>) {
+  const { eventId } = await ctx.params;
   try {
     const pool = await getConnection();
-    const result = await pool.request().query("SELECT * FROM Gallery");
+    const result = await pool.request().query(`SELECT * FROM Gallery WHERE ID = ${eventId}`);
     return NextResponse.json(result.recordset as GalleryModel[]);
   } catch (error: unknown) {
     const errorMessage = error instanceof Error ? error.message : "An unknown error occurred";
